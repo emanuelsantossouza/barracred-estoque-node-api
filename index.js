e27eaad13f365b9f514c7888bae363530c24cb0e
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-12-02 22:44:20 -0300

    Preparando para o primeiro deploy

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser"); // converso de formatos
 
-const routes = require("./routes/route");
-
 const server = express();
 server.use(cors());
 server.use(bodyParser.urlencoded({ extended: false }));
-
-server.use("/api", routes);
 
 
 module.exports = server;
