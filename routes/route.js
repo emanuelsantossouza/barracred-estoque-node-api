23dce86dc33db54505ec4812709553939b131c8c
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-12-02 23:11:27 -0300

    Preparando para o primeiro deploy

--- routes/route.js
+++ routes/route.js
@@ -4,12 +4,6 @@ const routes = require("express").Router();
 
 const pessoa = require("./pessoaRota");
 
-routes.get("/", async function (req, res) {
-    //homepage route returns some HTML
-    res.send(`<h1>Reached home!</h1> 
-            <br>
-            <a href='/carros'>Carros</a>`);
-});
 
 routes.use("/", pessoa);
 
